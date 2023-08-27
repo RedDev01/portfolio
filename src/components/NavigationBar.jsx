ce46af119d5e910c088119814a15085184c654a4
RedDev01 <larrieadriano@gmail.com>
2023-08-27 08:50:54 +0800

    add navigation bar in mobile view

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,6 +1,7 @@
 import '../App.css' 
 import {Link} from 'react-scroll'
 import {BiMenu} from 'react-icons/bi'
+import { useEffect, useState } from 'react';
 
  
 const NavigationBar = () => {
@@ -10,13 +11,20 @@ const NavigationBar = () => {
     const duration= 800
     const spy = true
     const smooth = true
+
+    const [navOpen, setNavOpen] = useState(false);
+
+    const openNav = () => {
+      setNavOpen(prevNavOpen => !prevNavOpen);
+    };
     
     return (
         <>
 
-        <div className='navicon'><BiMenu /></div>
+        <div className='navicon' onClick={openNav}><BiMenu /></div>
 
-        <nav className='navbar'>
+        <nav className={`navbar ${navOpen ? 'active' :''}`}>
+        <div className='navclose' onClick={openNav}>X</div>
         
         <Link className='navcontent' to='firstID' smooth={smooth} spy={spy} offset={offset} delay={delay} duration={duration}>
             Introduction
